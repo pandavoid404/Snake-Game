a5a8b9ddfca63bac461b61c104cb96c1c2e34d12
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-14 11:25:44 +0100

    basic non working frame

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JFrame;
+
+public class Main {
+    static short height = 1000;
+    static short width = 300; 
+    static JFrame frame = new JFrame("SnakeGame");
+    public static void main(String[]args) {
+        frame.setSize(width,height);
+    }
+
+} 
