170ed53df0ce31ae46ad7e253d77a9d4340d38b9
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-14 11:29:53 +0100

    working basic frame

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@
 import javax.swing.JFrame;
 
 public class Main {
-    static short height = 1000;
-    static short width = 300; 
+    static short height = 700;
+    static short width = 1000; 
     static JFrame frame = new JFrame("SnakeGame");
     public static void main(String[]args) {
+        //set size and closing
         frame.setSize(width,height);
-    }
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
 } 
