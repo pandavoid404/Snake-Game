da5c2422ab3b2f403b963a1bf41bcd43ef399516
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-14 12:01:52 +0100

    basic label added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
+    static JLabel label1;
     static short height = 700;
     static short width = 1000; 
     static JFrame frame = new JFrame("SnakeGame");
@@ -10,6 +12,10 @@ public class Main {
         frame.setSize(width,height);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
+        JLabel label=new JLabel();
+        label.setText("hello world");
+        label.setLocation(100,10);
+        frame.add(label);
     }
 } 
