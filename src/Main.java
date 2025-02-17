c2f838a3b4cd68eedc2fce49c380d65bf9d3076a
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-17 11:07:19 +0100

    titel homescreen

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.*;
 
 public class Main {
-    static JLabel label1;
     static short height = 700;
     static short width = 1000; 
     static JFrame frame = new JFrame("SnakeGame");
     public static void main(String[]args) {
         //set size and closing
         frame.setSize(width,height);
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        frame.setLayout(new GridBagLayout());
+
         JLabel label=new JLabel();
-        label.setText("hello world");
-        label.setLocation(100,10);
-        frame.add(label);
+        label.setText("Snake Game");
+        
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 0, 500, 0);  
+        gbc.gridx = 0;  
+        gbc.gridy = 10;  
+        gbc.anchor = GridBagConstraints.NORTH;
+
+        
+        frame.add(label,gbc);
+        frame.setVisible(true);
     }
 } 
