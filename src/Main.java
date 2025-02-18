a89bebde18cf5fbf63a5da686a6b8e4f7b9f9266
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-18 12:25:04 +0100

    added commends

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,33 @@ public class Main {
     static short width = 1000; 
     static JFrame frame = new JFrame("SnakeGame");
     public static void main(String[]args) {
-        //set size and closing
+        //exit and closing 
         frame.setSize(width,height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridBagLayout());
+        //call functions 
         createlabel( 0 ,  0 , "SnakeGame" );
         createbuton(0 , 10 , "StartGame",Color.BLACK, Color.WHITE);
         createbuton(0,20,"Settings" ,Color.BLACK, Color.WHITE);
         createbuton( 0 , 30 , "Quit Game",Color.BLACK, Color.WHITE );
     }
+    //create function for create a label for text
     public static  void createlabel(int gridx, int gridy ,String labeltext) {
         JLabel label=new JLabel(labeltext);
+        //add GridBagConstraints and add padding 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 0, 10, 0);  
         gbc.gridx = gridx;  
-        gbc.gridy = gridy;  
+        gbc.gridy = gridy; 
+
         frame.add(label,gbc);
         frame.setVisible(true);
     }
+    //create function for create a button 
     public static void createbuton(int gridx, int gridy ,String buttontext , Color bgColor, Color textColor ){
         GridBagConstraints gbc = new GridBagConstraints();
         JButton createbuton = new JButton(buttontext);
+        //set size , color and pading
         createbuton.setPreferredSize(new Dimension(200, 50));
         createbuton.setBackground(bgColor);
         createbuton.setForeground(textColor);
