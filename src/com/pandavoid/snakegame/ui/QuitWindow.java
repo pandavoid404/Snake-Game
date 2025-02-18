58867c9ea41e83477076eaed3ffc49b52535beee
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-18 19:52:48 +0100

    set quit buttons on x

--- src/com/pandavoid/snakegame/ui/QuitWindow.java
+++ src/com/pandavoid/snakegame/ui/QuitWindow.java
@@ -14,7 +14,7 @@ public class QuitWindow {
 		window.HideControls(true);
 		ActionListener CancelListener = e-> Cancel();
 		window.CreateButton( 0 , 40 , "Quit Game", Color.BLACK, Color.WHITE , QuitGameListener);
-		window.CreateButton( 0 , 80 , "Cancel", Color.BLACK, Color.WHITE , CancelListener);
+		window.CreateButton( 10 , 40 , "Cancel", Color.BLACK, Color.WHITE , CancelListener);
 		//TODO: add buttons here
 		window.ShowDisplay();
 	}
