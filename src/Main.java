8705108e322ae4b3cfd8bdb31fffe3fa289a0180
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-18 14:36:59 +0100

    functions for Button event actions

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Main {
-    static short height = 700;
-    static short width = 1000; 
+    static final short height = 700;
+    static final short width = 1000; 
     static JFrame frame = new JFrame("SnakeGame");
     public static void main(String[]args) {
         //exit and closing 
@@ -13,20 +13,20 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridBagLayout());
         //call functions 
-        create_label( 0 ,  0 , "SnakeGame" );
+        create_label( 0, 0, "SnakeGame");
 
-        ActionListener quitAction = e -> System.exit(0);
-        ActionListener CreditsAction = e -> System.out.println("Credits");
-        ActionListener SettingsAction = e -> System.out.println("settings");
-        ActionListener StartGameAction = e -> System.out.println("Game Starting...");
-        
-        createButton(0 , 10 , "StartGame",Color.BLACK, Color.WHITE,StartGameAction);
+        ActionListener quitAction = e -> quitGame();
+        ActionListener CreditsAction = e -> Credits();
+        ActionListener SettingsAction = e -> Settings();
+        ActionListener StartGameAction = e -> StartGame();
+
+        createButton(0 , 10 , "StartGame",Color.BLACK, Color.WHITE, StartGameAction);
         createButton(0,20,"Settings" ,Color.BLACK, Color.WHITE, SettingsAction);
-        createButton( 0 , 30 , "Credits",Color.BLACK, Color.WHITE ,CreditsAction);
-        createButton( 0 , 40 , "Quit Game",Color.BLACK, Color.WHITE ,quitAction);
+        createButton( 0 , 30 , "Credits",Color.BLACK, Color.WHITE , CreditsAction);
+        createButton( 0 , 40 , "Quit Game",Color.BLACK, Color.WHITE , quitAction);
     }
     //create function for create a label for text
-    public static  void create_label(int gridx, int gridy ,String label_text) {
+    private  static  void create_label(int gridx, int gridy ,String label_text) {
         JLabel label = new JLabel(label_text);
         //add GridBagConstraints and add padding 
         GridBagConstraints gbc = new GridBagConstraints();
@@ -38,7 +38,7 @@ public class Main {
         frame.setVisible(true);
     }
     //create function for create a button 
-    public static void createButton(int gridx, int gridy ,String buttontext , Color bgColor, Color textColor , ActionListener action){
+    private static void createButton(int gridx, int gridy ,String buttontext , Color bgColor, Color textColor , ActionListener action){
         GridBagConstraints gbc = new GridBagConstraints();
         JButton create_button = new JButton(buttontext);
         //set size ,color and padding
@@ -53,5 +53,17 @@ public class Main {
         gbc.fill = GridBagConstraints.HORIZONTAL;
         frame.add(create_button,gbc);
         frame.setVisible(true);
+    }
+    private static void quitGame(){
+        System.exit(0);
+    }
+    private static void Credits(){
+        System.out.println("Credits");
+    }
+    private static void Settings(){
+        System.out.println("settings");
+    }
+    private static void StartGame(){
+        System.out.println("Game Starting...");
     }
 } 
