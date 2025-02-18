b5ba7a88a90b0a05ecf42cb0717c2461803de02a
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-18 20:17:13 +0100

    posioning quit menu

--- src/com/pandavoid/snakegame/ui/QuitWindow.java
+++ src/com/pandavoid/snakegame/ui/QuitWindow.java
@@ -5,13 +5,13 @@ import java.awt.event.ActionListener;
 
 public class QuitWindow {
 	private final Window window;
-	private static final short height = 200;
+	private static final short height = 300;
 	private static final short width = 400;
 	public QuitWindow(ActionListener QuitGameListener,Point MainWindowPos,int MainWindowWidth,int MainWindowHeight) {
 		System.out.println(MainWindowPos);
 		window = new Window("SnakeGame", width, height);
 		window.SetBGColor(Color.BLUE);
-		window.SetPosition(new Point(MainWindowPos.x+MainWindowWidth/2-width/2,MainWindowPos.y+MainWindowHeight/2-height/2));
+		window.SetPosition(new Point(MainWindowPos.x+MainWindowWidth/2-width/2,MainWindowPos.y+MainWindowHeight/2-height/3));
 		window.HideControls(true);
 		ActionListener CancelListener = e-> Cancel();
 		window.CreateButton( 0 , 40 , "Quit Game", Color.BLACK, Color.WHITE , QuitGameListener);
