c5de95560c54fdb67001d29f78b7447845b2ec8b
André Verweij <51133@gmail.com>
2025-02-18 21:29:18 +0100

    added files for game logic

--- src/com/pandavoid/snakegame/logic/Board.java
+++ src/com/pandavoid/snakegame/logic/Board.java
@@ -1,7 +1,14 @@
 package com.pandavoid.snakegame.logic;
 
 public class Board {
+	private Cell[][] board;
 	public Board(int height, int width) {
-
+		board = new Cell[height][width];
+		for (int RI = 0; RI < height;RI++) {
+			for (int CI = 0; CI < width;CI++) {
+				Cell cell = new Cell();
+				board[RI][CI] = cell;
+			}
+		}
 	}
 }
---
+++ src/com/pandavoid/snakegame/logic/SnakeCell.java
@@ -0,0 +1,4 @@
+package com.pandavoid.snakegame.logic;
+
+public class SnakeCell {
+}
