2c49544011e79831226eeab8e0a48cd2741267e4
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-18 14:48:33 +0100

    window create in window

--- src/Main.java
+++ src/Main.java
@@ -25,35 +25,6 @@ public class Main {
         createButton( 0 , 30 , "Credits",Color.BLACK, Color.WHITE , CreditsAction);
         createButton( 0 , 40 , "Quit Game",Color.BLACK, Color.WHITE , quitAction);
     }
-    //create function for create a label for text
-    private  static  void create_label(int gridx, int gridy ,String label_text) {
-        JLabel label = new JLabel(label_text);
-        //add GridBagConstraints and add padding 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 0, 10, 0);  
-        gbc.gridx = gridx;  
-        gbc.gridy = gridy; 
-
-        frame.add(label,gbc);
-        frame.setVisible(true);
-    }
-    //create function for create a button 
-    private static void createButton(int gridx, int gridy ,String buttontext , Color bgColor, Color textColor , ActionListener action){
-        GridBagConstraints gbc = new GridBagConstraints();
-        JButton create_button = new JButton(buttontext);
-        //set size ,color and padding
-        create_button.setPreferredSize(new Dimension(200, 50));
-        create_button.setBackground(bgColor);
-        create_button.setForeground(textColor);
-        create_button.setFocusPainted(false);
-        create_button.addActionListener(action);
-        gbc.insets = new Insets(10, 10, 10, 10);
-        gbc.gridx = gridx;  
-        gbc.gridy = gridy;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        frame.add(create_button,gbc);
-        frame.setVisible(true);
-    }
     private static void quitGame(){
         System.exit(0);
     }
--- src/window.java
+++ src/window.java
@@ -1,3 +1,34 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
 public class window {
-    
+        //create function for create a label for text
+    public static  void create_label(int gridx, int gridy ,String label_text) {
+        JLabel label = new JLabel(label_text);
+        //add GridBagConstraints and add padding 
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 0, 10, 0);  
+        gbc.gridx = gridx;  
+        gbc.gridy = gridy; 
+
+        frame.add(label,gbc);
+        frame.setVisible(true);
+    }
+    //create function for create a button 
+    public static void createButton(int gridx, int gridy ,String buttontext , Color bgColor, Color textColor , ActionListener action){
+        GridBagConstraints gbc = new GridBagConstraints();
+        JButton create_button = new JButton(buttontext);
+        //set size ,color and padding
+        create_button.setPreferredSize(new Dimension(200, 50));
+        create_button.setBackground(bgColor);
+        create_button.setForeground(textColor);
+        create_button.setFocusPainted(false);
+        create_button.addActionListener(action);
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.gridx = gridx;  
+        gbc.gridy = gridy;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        frame.add(create_button,gbc);
+        frame.setVisible(true);
+    }
 }
