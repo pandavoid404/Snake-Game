7fbd1ae31cf26fb1b0ccc61633e9d6727a722a70
André Verweij <51133@gmail.com>
2025-02-19 19:50:32 +0100

    basic snake input

--- src/com/pandavoid/snakegame/game/snake/Snake.java
+++ src/com/pandavoid/snakegame/game/snake/Snake.java
@@ -1,4 +1,13 @@
 package com.pandavoid.snakegame.game.snake;
 
+import com.pandavoid.snakegame.game.Game;
+import com.pandavoid.snakegame.game.config.PlayerConfig;
+
 public class Snake {
+	private Game game;
+	private PlayerConfig config;
+	public Snake(Game game, PlayerConfig config) {
+		this.game = game;
+		this.config = config;
+	}
 }
