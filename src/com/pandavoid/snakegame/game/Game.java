063ec55ca2cbd9d24dc233f3dd2f3371fed74994
André Verweij <51133@gmail.com>
2025-02-19 19:56:30 +0100

    added parameters to the snake instance

--- src/com/pandavoid/snakegame/game/Game.java
+++ src/com/pandavoid/snakegame/game/Game.java
@@ -2,11 +2,12 @@ package com.pandavoid.snakegame.game;
 
 import com.pandavoid.snakegame.game.board.Board;
 import com.pandavoid.snakegame.game.config.GameConfig;
+import com.pandavoid.snakegame.game.config.PlayerConfig;
 import com.pandavoid.snakegame.game.snake.Snake;
 
 public class Game {
 	public Game(GameConfig config) {
 		Board board = new Board(10, 20);
-		Snake snake = new Snake();
+		Snake snake = new Snake(this,new PlayerConfig());
 	}
 }
