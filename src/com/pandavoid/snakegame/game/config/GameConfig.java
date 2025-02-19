f9db7fdb5fe671d49eccc31c3b0aa3d95d3a70d0
André Verweij <51133@gmail.com>
2025-02-19 12:23:16 +0100

    added configs

---
+++ src/com/pandavoid/snakegame/game/config/GameConfig.java
@@ -0,0 +1,25 @@
+package com.pandavoid.snakegame.game.config;
+
+public class GameConfig {
+	private int players;
+	private final PlayerConfig[] playerConfigs = new PlayerConfig[2];
+	public GameConfig() {
+		players = 1;
+	}
+	public void setPlayers(int amount) {
+		players = amount;
+		playerConfigs[0] = new PlayerConfig();
+		if (players == 1) {
+			playerConfigs[1] = null;
+		} else if (players == 2) {
+			playerConfigs[1] = new PlayerConfig();
+		}
+	}
+	public int getPlayers() {
+		return players;
+	}
+
+	public PlayerConfig getPlayerConfigs(int player) {
+		return playerConfigs[player];
+	}
+}
---
+++ src/com/pandavoid/snakegame/game/config/PlayerConfig.java
@@ -0,0 +1,13 @@
+package com.pandavoid.snakegame.game.config;
+
+import java.awt.*;
+
+public class PlayerConfig {
+	private Color color;
+	public PlayerConfig() {
+		color = Color.BLUE;
+	}
+	public void SetColor(Color newColor) {
+		color = newColor;
+	}
+}
