0c50b9d1392a26c5ded14af7ab0fd95fc726e6a5
Martien van den Brink <martienvandenbrink@gmail.com>
2025-02-21 12:09:53 +0100

    corect size gameboard

--- src/com/pandavoid/snakegame/ui/window/GamePanel.java
+++ src/com/pandavoid/snakegame/ui/window/GamePanel.java
@@ -3,7 +3,7 @@ package com.pandavoid.snakegame.ui.window;
 class GamePanel {
 	private final ImagePanel panel;
 	public GamePanel() {
-		panel = new ImagePanel("assets/defultbackgroundsnakegameboard.png", 600 , 400 );
+		panel = new ImagePanel("assets/defultbackgroundsnakegameboard.png", 1200 , 700 );
 		panel.CreateLabel("Snake Game");
 		}
 	public ImagePanel getPanel() {
