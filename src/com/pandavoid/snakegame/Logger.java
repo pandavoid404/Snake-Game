9a5398b95f284cf2d1e82ce2407582941ed9ef16
André Verweij <51133@gmail.com>
2025-06-04 18:49:41 +0200

    better log formatting

--- src/com/pandavoid/snakegame/Logger.java
+++ src/com/pandavoid/snakegame/Logger.java
@@ -4,14 +4,18 @@ import com.pandavoid.snakegame.enums.LogLevel;
 import com.pandavoid.snakegame.enums.LogType;
 
 import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Logger {
-	private static final List<LogType> EnabledDebugLogTypes = List.of();
+	private static final List<LogType> EnabledDebugLogTypes = List.of(LogType.TICK,LogType.DISPLAY);
 
 	private static void sendLog(LogType type, LogLevel level, String message) {
-		System.out.println("[SnakeGame] " + type + " " + level + ": " + message);
+		LocalDateTime now = LocalDateTime.now();
+		String formatted = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+		System.out.println("[SnakeGame] [" + formatted + "] [" + type + "] " + level + ": " + message);
 	}
 	public static void debug(LogType type, String message) {
 		if (EnabledDebugLogTypes.contains(type)) {
